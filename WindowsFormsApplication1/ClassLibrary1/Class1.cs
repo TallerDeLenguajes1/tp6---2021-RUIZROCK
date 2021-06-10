4f17bc5275ee7de45a256c55d2bba8352451497e
RUIZROCK <sergioruizrock@gmail.com>
2021-06-10 19:59:48 -0300

    tp realizado con windows forms (perdon la demora, tuve problemas con la compu ademas de haberme atrasado conceptualmente)

---
+++ WindowsFormsApplication1/ClassLibrary1/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+
+    class operaciones
+    {
+        public bool pasaje (string num)
+        {
+            try
+            {
+                double n = Convert.ToDouble(num);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public double sumar(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double resta(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double producto(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double division(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
