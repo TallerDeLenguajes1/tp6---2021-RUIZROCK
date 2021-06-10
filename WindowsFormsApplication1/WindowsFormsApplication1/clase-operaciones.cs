63f81eee5a9a51e3beb1f4730cc359ae75a5f453
RUIZROCK <sergioruizrock@gmail.com>
2021-06-10 20:18:24 -0300

    tp realizado con windows form. perdon por el atraso.

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/clase-operaciones.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+
+    //se realiza la clase para poder realizar las operaciones de la calculadora.
+
+    class operaciones
+    {
+        public double sumar(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double resta(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double producto(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double division(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
