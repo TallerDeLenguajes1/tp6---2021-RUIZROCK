5e17d25b21c6fb43f43f5c9d27e1c2d9b13fc1d8
RUIZROCK <sergioruizrock@gmail.com>
2021-06-10 22:00:40 -0300

    Actualizacion, el textbox estaba transparente.

--- Calculadora/Calculadora/Form1.Designer.cs
+++ Calculadora/Calculadora/Form1.Designer.cs
@@ -276,7 +276,7 @@
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Calculadora";
             this.Text = "Calculadora";
-            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
             this.ResumeLayout(false);
             this.PerformLayout();
 
